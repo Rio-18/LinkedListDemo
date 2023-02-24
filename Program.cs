e48ff1fff16e2828f0a6f92ec544e29d1223aac7
riya more <you@example.com>
2023-02-24 23:57:24 +0530

    first commit

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿namespace LinkedListDemo
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            LinkedList linked = new LinkedList();
+            linked.Add(20);
+            linked.Add(20);
+            linked.Add(60);
+            linked.Add(80);
+
+            linked.Display();
+        }
+    }
+}
