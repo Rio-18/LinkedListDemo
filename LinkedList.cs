afdbc3731e765dc9cbabd02fc45dfcc8f2c03cca
riya more <you@example.com>
2023-03-08 16:39:54 +0530

    Create Linked List

--- LinkedList.cs
+++ LinkedList.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using LinkedListDemo;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,13 +10,9 @@ namespace LinkedListDemo
     public class LinkedList
     {
         public Node head;
-
         public void Add(int data)
         {
-            Node node = new Node();
-         
-          
-
+            Node node = new Node(data);
             if (this.head == null)
             {
                 this.head = node;
@@ -25,21 +22,25 @@ namespace LinkedListDemo
                 Node temp = head;
                 while (temp.next != null)
                 {
-                    temp=temp.next;
-
+                    temp = temp.next;
                 }
                 temp.next = node;
             }
-            Console.WriteLine("{0} inserted into linked list", node.data);
+            Console.WriteLine("{0} inserted into linked list ", node.data);
         }
-        public void Display() 
+        public void Display()
         {
-            Node temp=this.head;
-            if(temp==null)
+            Node temp = this.head;
+            if (temp == null)
             {
-                Console.WriteLine("Linkedlist is Empty");
+                Console.WriteLine("Linked list is empty");
+            }
+            Console.WriteLine("Linked list is: ");
+            while (temp != null)
+            {
+                Console.WriteLine(temp.data + " ");
+                temp = temp.next;
             }
         }
     }
 }
-
--- Program.cs
+++ Program.cs
@@ -2,15 +2,30 @@
 {
     class Program
     {
-        public static void Main(string[] args)
+        static void Main(string[] args)
         {
-            LinkedList linked = new LinkedList();
-            linked.Add(20);
-            linked.Add(20);
-            linked.Add(60);
-            linked.Add(80);
-
-            linked.Display();
+            LinkedList linkedList = new LinkedList();
+            bool flag = true;
+            while (flag)
+            {
+                Console.WriteLine("Enter Option");
+                Console.WriteLine("1:Create linked list\n2: Display ");
+                int option = Convert.ToInt32(Console.ReadLine());
+                switch (option)
+                {
+                    case 1:
+                        linkedList.Add(56);
+                        linkedList.Add(30);
+                        linkedList.Add(70);
+                        break;
+                    case 2:
+                        linkedList.Display();
+                        break;
+                    default:
+                        Console.WriteLine("Enter Correct Option");
+                        break;
+                }
+            }
         }
     }
 }
