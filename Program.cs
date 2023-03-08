662c54829e4542390e8bb871c883fffb872b8df4
riya more <you@example.com>
2023-03-08 17:22:46 +0530

    Remove First Element

--- Program.cs
+++ Program.cs
@@ -12,6 +12,8 @@
                 Console.WriteLine("1:Create linked list");
                 Console.WriteLine("2:Display");
                 Console.WriteLine("3:ReverseContact");
+                Console.WriteLine("4:Append");
+                Console.WriteLine("5:Remove First Node");
                 int option = Convert.ToInt32(Console.ReadLine());
                 switch (option)
                 {
@@ -27,6 +29,17 @@
                         linkedList.InReverse(56);
                         linkedList.InReverse(30);
                         linkedList.InReverse(70);
+                        break;
+                    case 4:
+                        linkedList.Append(56);
+                        linkedList.Append(30);
+                        linkedList.Append(70);
+                        break;
+                    case 5:
+                        linkedList.Add(56);
+                        linkedList.Add(30);
+                        linkedList.Add(70);
+                        linkedList.RemoveFirstNode();
                         break;
                     default:
                         Console.WriteLine("Enter correct option");
---
+++ RemoveFirst.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedListDemo
+{
+    public class RemoveFirst
+    {
+        public Node head;
+        public void Add(int data)
+        {
+            Node node = new Node(data);
+            if (this.head == null)
+            {
+                this.head = node;
+            }
+            else
+            {
+                Node temp = head;
+                while (temp.next != null)
+                {
+                    temp = temp.next;
+                }
+                temp.next = node;
+            }
+            Console.WriteLine("{0} Values Are Inserted", node.data);
+        }
+        public void AddFirst(int data)
+        {
+            Node node = new Node(data);
+            node.next = this.head;
+            this.head = node;
+            Console.WriteLine("{0} inserted into linked list ", node.data);
+        }
+        public void Display()
+        {
+            Node temp = this.head;
+            if (temp == null)
+            {
+                Console.WriteLine("Liked List Is Empty");
+            }
+            Console.WriteLine("Linked list IS :");
+            while (temp != null)
+            {
+                Console.WriteLine(temp.data + "");
+                temp = temp.next;
+            }
+        }
+
+        public void InReverse(int data)
+        {
+            Node node = new Node(data);
+            if (head == null)
+            {
+                head = node;
+            }
+            else
+            {
+                Node temp = head;
+                node.next = temp;
+                head = node;
+            }
+            Console.WriteLine($"{node.data} ");
+        }
+        public void Append(int data)
+        {
+            Node node = new Node(data);
+            if (head != null)
+            {
+                Node temp = head;
+                while (temp.next != null)
+                {
+                    temp = temp.next;
+                }
+                temp.next = node;
+                Console.WriteLine($"{node.data} is added after the {temp.data}");
+            }
+            else { head = node; }
+        }
+        public void RemoveFirstNode()
+        {
+            if (head == null)
+            {
+                Console.WriteLine("linked list is empty");
+            }
+            this.head = this.head.next;
+            Console.WriteLine("the first element is deleted");
+        }
+    }
+}
